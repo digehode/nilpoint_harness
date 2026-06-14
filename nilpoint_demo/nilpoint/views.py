ec4f92ff09700b781d19b44c41bcea131285d871
James Shuttleworth <digehode@googlemail.com>
2026-06-14 19:39:11 +0100

    Moved to having final render dealt with by the NilpointView superclass.
    Now context can be expanded before render to include game, etc.

--- nilpoint_demo/nilpoint/views.py
+++ nilpoint_demo/nilpoint/views.py
@@ -19,10 +19,9 @@ class NilpointGameBasic(View):
         super().__init__(*args, **kwargs)
 
     def get_player_character(self, request, *args, **kwargs):
-        if self.game is None:
-            raise Exception("Request for user, with no curent game set in the view")
-        if request.user and not request.user.is_anonymous:
+        if self.game is not None and request.user and not request.user.is_anonymous:
             return self.game.get_player_character(request.user)
+        return None
 
     def get_game_object(self, request, *args, **kwargs):
         """Return the generic game object associated with this request.
@@ -39,10 +38,36 @@ class NilpointGameBasic(View):
         Raises nilpoint.MissingSlugException if the slug doesn't identify a game instance
 
         """
+
         if "nilpoint_slug" in kwargs:
             return Game.objects.get(nilpoint_slug=kwargs["nilpoint_slug"])
         else:
             raise NilpointMissingSlugException("No nilpoint_slug was given in kwargs")
+
+    def get_context_data(self, request, *args, **kwargs):
+        context = {}
+
+        try:
+            self.game = self.get_game_object(request, *args, **kwargs)
+        except Game.DoesNotExist:
+            self.game = None
+
+        except NilpointMissingSlugException:
+            self.game = None
+
+        self.player_character = self.get_player_character(request, *args, **kwargs)
+        context["game"] = self.game
+        context["player_character"] = self.player_character
+
+        return context
+
+    def nilpoint_render(self, request, template, context={}, *args, **kwargs):
+        full_context = {}
+
+        full_context.update(self.get_context_data(request, *args, **kwargs))
+        full_context.update(context)
+
+        return render(request, template, context=full_context)
 
     def get(self, request, *args, **kwargs):
         """GET dispatcher
@@ -51,42 +76,24 @@ class NilpointGameBasic(View):
 
         Ensures the self.game object is set to the downcast game in question"""
 
-        # Retrieve the generic game object
-        try:
-            self.game = self.get_game_object(request, *args, **kwargs)
-        except Game.DoesNotExist:
-            return HttpResponseNotFound(
-                f"We don't seem to have a game usingthe slig '{kwargs['nilpoint_slug']}'!"
-            )
-        except NilpointMissingSlugException:
-            return HttpResponseNotFound("No nilpoint slug provided!")
-
-        self.player_character = self.get_player_character(request, *args, **kwargs)
-
         action = request.GET.get("action", None)
         if action is None or action not in self._handlers:
             return HttpResponse("Action required", content_type="text/plain")
 
-        return getattr(self, self._handlers[action])(request, args, kwargs)
+        response = getattr(self, self._handlers[action])(request, *args, **kwargs)
+        return response
 
     def handle_overview(self, request, *args, **kwargs):
         """Page overview
 
         Override the 'partial' used to render the content.
         """
+
         if hasattr(self, "overview_partial"):
             partial = self.overview_partial
         else:
             partial = "nilpoint/game_overview.jinja2#game_overview"
-        # TODO: move the adding of game and user to context into a single location
-        return render(
-            request,
-            partial,
-            context={
-                "game": self.game.get_real_instance(),
-                "player_character": self.player_character,
-            },
-        )
+        return self.nilpoint_render(request, partial, context={}, *args, **kwargs)
 
     def debug(self, request, *args, **kwargs):
         """Debug view, can be used to drop in to places before the
@@ -94,26 +101,33 @@ class NilpointGameBasic(View):
         expected
 
         """
+        return self.nilpoint_render(
+            request,
+            "nilpoint/debug.jinja2#debug",
+            context={"request": request, "args": args, "kwargs": kwargs},
+            *args,
+            **kwargs,
+        )
 
-        content = "Nilpoint Debug Page\n"
-        content += "===================\n\n"
-        content += f"Working on game: {self.game}\n\n"
-        content += f"Current player character: {self.player_character}\n\n"
-        content += f"{request.method} : {request.path}\n\n"
-        content += "*args\n-----\n\n"
-        for i in args:
-            content += f"    {i}\n"
-        content += "\n"
-        content += "**kwargs\n--------\n\n"
+        # content = "Nilpoint Debug Page\n"
+        # content += "===================\n\n"
+        # content += f"Working on game: {self.game}\n\n"
+        # content += f"Current player character: {self.player_character}\n\n"
+        # content += f"{request.method} : {request.path}\n\n"
+        # content += "*args\n-----\n\n"
+        # for i in args:
+        #     content += f"    {i}\n"
+        # content += "\n"
+        # content += "**kwargs\n--------\n\n"
 
-        for k in kwargs:
-            content += f"    {k} : {kwargs[k]}\n"
-        content += "\n"
-        content += f"GET: {request.GET}\n\n"
-        content += f"POST: {request.POST}\n\n"
-        content += f"COOKIES: {request.COOKIES}\n\n"
-        content += f"USER: {request.user}\n\n"
-        return HttpResponse(content, content_type="text/plain")
+        # for k in kwargs:
+        #     content += f"    {k} : {kwargs[k]}\n"
+        # content += "\n"
+        # content += f"GET: {request.GET}\n\n"
+        # content += f"POST: {request.POST}\n\n"
+        # content += f"COOKIES: {request.COOKIES}\n\n"
+        # content += f"USER: {request.user}\n\n"
+        # return HttpResponse(content, content_type="text/plain")
 
 
 class NilpointRootView(View):
